23bc8644b6992dff8d5fbd8e466423b1d8d7a137
Ilya Krestsov <krestsov.iv@yandex.ru>
2025-06-29 23:31:38 +0200

    bugfix no base URI for restassured

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -5,6 +5,7 @@ import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import io.restassured.RestAssured;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -17,6 +18,8 @@ public class TestBase {
     @BeforeAll
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
+        RestAssured.baseURI = "https://demoqa.com";
+
         Configuration.pageLoadStrategy = "eager";
 
         Configuration.browser = "chrome";
